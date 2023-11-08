4390ac29b91a4d1b5205de4e54ab3f853fa0f79a
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-11-08 13:20:40 +0300

    feat: Fix spacing on welcome screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -11,7 +11,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
-              flex: 3,
+              flex: 4,
               child: Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
@@ -23,54 +23,56 @@ class MyHomePage extends StatelessWidget {
             ),
             Expanded(
               flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Column(children: [
-                  const SizedBox(height: 50),
-                  const Text(
-                    "EyePhone",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 24.0,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Column(children: [
+                    // const SizedBox(height: 50),
+                    const Text(
+                      "EyePhone",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24.0,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  const Text(
-                    "See the world through the eyes of your phone",
-                    style: TextStyle(
-                      fontSize: 16.0,
+                    const SizedBox(height: 15),
+                    const Text(
+                      "See the world through the eyes of your phone",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 35),
-                  FittedBox(
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        margin: const EdgeInsets.only(bottom: 25),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 26, vertical: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          color: const Color(0xFFB99889),
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Text(
-                              "Start exploring",
-                              style: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            const SizedBox(width: 10),
-                            const Icon(
-                              Icons.arrow_forward,
-                              color: Colors.white,
-                            )
-                          ],
+                    const SizedBox(height: 35),
+                    FittedBox(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(bottom: 25),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 26, vertical: 16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: const Color(0xFFB99889),
+                          ),
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                "Start exploring",
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                              const SizedBox(width: 10),
+                              const Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ]),
+                  ]),
+                ),
               ),
             )
           ],
