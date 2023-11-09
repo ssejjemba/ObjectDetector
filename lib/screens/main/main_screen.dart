ee976aec08ed86338eddddb7ca70a4c3eb5ebb3c
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-11-10 02:30:35 +0300

    Fix android build and display screens

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:cats_and_dogs/common/loading/loading_widget.dart';
+import 'package:cats_and_dogs/helper/classifier.dart';
+import 'package:cats_and_dogs/screens/camera/camera_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -16,9 +21,31 @@ class _MainScreenState extends State<MainScreen> {
 
   bool _isLoading = false;
 
+  ImageClassificationHelper? imageClassificationHelper;
+
+  Map<String, double>? classification;
+
+  List<Widget>? classificationWidgets;
+
+  late CameraDescription cameraDescription;
+
+  bool cameraIsAvailable = Platform.isAndroid || Platform.isIOS;
+
   @override
   void initState() {
     super.initState();
+    imageClassificationHelper = ImageClassificationHelper();
+    imageClassificationHelper!.initHelper();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      initCamera();
+    });
+  }
+
+  initCamera() async {
+    if (cameraIsAvailable) {
+      // get list available camera
+      cameraDescription = (await availableCameras()).first;
+    }
   }
 
   Future _getImage(ImageSource source) async {
@@ -64,14 +91,67 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  _detectObjectsInImage() {
+  _processImage() {
     if (_image == null) {
       return;
     }
 
-    setState(() {
-      _isLoading = true;
-    });
+    _isLoading = true;
+    setState(() {});
+    _detectObjectsInImage();
+  }
+
+  Future<void> _detectObjectsInImage() async {
+    if (_image == null) {
+      return;
+    }
+
+    final imageData = await compute(_readImageBytes, _image!);
+    final image = img.decodeImage(imageData as Uint8List);
+    classification = await imageClassificationHelper?.inferenceImage(image!);
+    if (classification != null) {
+      classificationWidgets = [
+        ...(classification!.entries.toList()
+              ..sort(
+                (a, b) => a.value.compareTo(b.value),
+              ))
+            .reversed
+            .take(3)
+            .map(
+              (e) => Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.transparent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      e.key,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      e.value.toStringAsFixed(2),
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+      ];
+    }
+    _isLoading = false;
+    debugPrint(classification.toString());
+    setState(() {});
+  }
+
+  static Future<List<int>> _readImageBytes(File image) async {
+    return await image.readAsBytes();
   }
 
   Widget _buildLoader() {
@@ -110,7 +190,7 @@ class _MainScreenState extends State<MainScreen> {
                   ),
                 ),
                 IconButton(
-                  onPressed: _detectObjectsInImage,
+                  onPressed: _processImage,
                   icon: const Icon(
                     Icons.check,
                     color: Colors.white,
@@ -118,6 +198,15 @@ class _MainScreenState extends State<MainScreen> {
                   ),
                 )
               ],
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: Center(
+                child: classificationWidgets == null
+                    ? const SizedBox.shrink()
+                    : classificationWidgets![0],
+              ),
             ),
             Expanded(
               child: Padding(
@@ -146,12 +235,19 @@ class _MainScreenState extends State<MainScreen> {
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              _buildButton(Icons.camera_alt, "Camera",
-                                  () => _getImage(ImageSource.camera)),
-                              _buildButton(
-                                  Icons.photo_album_outlined,
-                                  "Gallery",
+                              _buildButton(Icons.photo, "Gallery",
                                   () => _getImage(ImageSource.gallery)),
+                              _buildButton(Icons.camera, "Camera", () {
+                                if (cameraIsAvailable) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => CameraScreen(
+                                          camera: cameraDescription),
+                                    ),
+                                  );
+                                }
+                              }),
                             ],
                           ),
                         )),
